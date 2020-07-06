1fc4dd6cd5204e20d1b6dd14e4587a271230ab0a
Muhammad Usman Arif <usmanarif83@gmail.com>
2020-07-06 17:28:35 +0500

    Update Routerscreen.js

--- src/screens/Routerscreen.js
+++ src/screens/Routerscreen.js
@@ -4,13 +4,13 @@ import Register from './Register';
 import Login from './Login';
 import Home from './HomeScreen';
 import Splash from './SplashScreen';
-import SecondarySplash from './SecondarySplashScreen';
+
 
 const RouterScreen = () => (
     <Router>
         <Stack key="root">
-            <Scene key="SecondarySplash" intial={true} component={SecondarySplash} hideNavBar={true}/>
-            <Scene key="Splash" component={Splash}   hideNavBar={true}/>
+           
+            <Scene key="Splash" component={Splash} intial={true}   hideNavBar={true}/>
             <Scene key="Login" component={Login} hideNavBar={true}/>
             <Scene key="Register" component=  {Register} hideNavBar={true}/>
             <Scene key="Home" component={Home} hideNavBar={true} />
